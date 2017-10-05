a69732b159e2c14db16537701812ad1e9fcb7e1f
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-05 22:56:46 +0100

    Cleaner code.

--- app.js
+++ app.js
@@ -1,76 +1,67 @@
-var Nightmare = require('nightmare');
+var nightmare = require('nightmare');
 var util = require('util');
 var moment = require('moment');
 var $ = require("jquery");
 var fs = require('fs');
 var schedule = require('node-schedule');
 
-
-
-var language = 'es';
+var language = "en";
 var searchTerm = "#bitcoin";
-var daysToCover = 1;
 var dateFrom = moment(new Date(2017, 9, 3));
 
 var baseUrl = "https://twitter.com/search?l=%s&q=%s&src=typd";
 
+var job = schedule.scheduleJob("*/10 * * * * *", function () {
 
-var j = schedule.scheduleJob('*/10 * * * * *', function()
-{
-
-    console.log(dateFrom.subtract(1, 'd'));
     var to = moment(dateFrom);
-    to.add(1, 'd')
-    var query = encodeURIComponent(util.format("%s since:%s until:%s",
-    searchTerm,
-    dateFrom.format("YYYY-MM-DD"),
-    to.format("YYYY-MM-DD")));
+    to.add(1, 'd');
+    dateFrom.subtract(1, "d");
 
+    var query = encodeURIComponent(util.format("%s since:%s until:%s",
+        searchTerm,
+        dateFrom.format("YYYY-MM-DD"),
+        to.format("YYYY-MM-DD")));
 
     var actualUrl = util.format(baseUrl,
         language,
         query);
 
-    console.log(moment());        
-    console.log("Executing: " + actualUrl);
-    
-    var nightmare = Nightmare({ show: false });
-    nightmare
+    var nightmareInstance = nightmare({ show: false });
+    nightmareInstance
         .goto(actualUrl)
-        .wait('.stream-items')
-        .evaluate(function(){
+        .wait(".stream-items")
+        .evaluate(function () {
             var tweets = [],
-            tweetDivs = $("div.tweet");
-            for(var i=0; i < tweetDivs.length; i++) {
+                tweetDivs = $("div.tweet");
+            for (var i = 0; i < tweetDivs.length; i++) {
                 var tweetDiv = $(tweetDivs[i]);
-                var tweet_id = tweetDivs.attr("data-tweet-id");
-                var screen_name = tweetDiv.find("span.username").text();
-                var full_name = tweetDiv.find("strong.fullname").text();
-                var time_stamp = tweetDiv.find("span._timestamp").attr("data-time");
-                var tweet_text = tweetDiv.find("p.tweet-text").text();
-                var raw_html = tweetDiv.find("p.tweet-text").html();
-                var tweet = 
-                { 
-                    tweetId: tweet_id, 
-                    screenName: screen_name, 
-                    fullName: full_name,
-                    tweetText: tweet_text,
-                    rawHtml: raw_html,
-                    timestamp: time_stamp
-                };
+                var tweetId = tweetDivs.attr("data-tweet-id");
+                var screenName = tweetDiv.find("span.username").text();
+                var fullName = tweetDiv.find("strong.fullname").text();
+                var timeStamp = tweetDiv.find("span._timestamp").attr("data-time");
+                var tweetText = tweetDiv.find("p.tweet-text").text();
+                var rawHtml = tweetDiv.find("p.tweet-text").html();
+                var tweet =
+                    {
+                        tweetId: tweetId,
+                        screenName: screenName,
+                        fullName: fullName,
+                        tweetText: tweetText,
+                        rawHtml: rawHtml,
+                        timestamp: timeStamp
+                    };
                 tweets.push(tweet);
             }
             return tweets;
         })
         .end()
-        .then(function(result){ 
-            fs.writeFile(dateFrom.format("YYYY-MM-DD") + "-tweets.json", JSON.stringify(result), function(err) {
-                if(err) {
+        .then(function (result) {
+            fs.writeFile(dateFrom.format("YYYY-MM-DD") + "-tweets.json", JSON.stringify(result), function (err) {
+                if (err) {
                     console.log(err);
-                    return console.log(err);
-                }else{
+                } else {
                     console.log("Data for " + dateFrom.format("YYYY-MM-DD") + " saved!");
                 }
-            }); 
+            });
         });
-    } );
+});
