a152acf2593d6b23e9939ce45e6b34a775433925
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-11-23 20:40:29 +0000

    Migration to a better crawling script.

--- JsCrawler/crawl.js
+++ JsCrawler/crawl.js
@@ -4,8 +4,10 @@ var moment = require("moment");
 var $ = require("jquery");
 var fs = require("fs");
 var schedule = require("node-schedule");
-var slugify = require('slug')
+var slugify = require('slug');
 var ArgumentParser = require('argparse').ArgumentParser;
+var path = require('path');
+
 
 var parser = new ArgumentParser({
   version: '0.0.1',
@@ -13,17 +15,71 @@ var parser = new ArgumentParser({
   description: 'Argparse example'
 });
 
-// Year, Month (zero based), Day
+parser.addArgument(
+    [ '-s', '--search_term' ],
+    {
+        help: 'The search term',
+        required: true
+    }
+);
+
+parser.addArgument(
+    [ '-l', '--language' ],
+    {
+        help: 'The language to run the search on',
+        required: false,
+        defaultValue: 'en'
+    }
+);
+
+parser.addArgument(
+    [ '-from', '--from' ],
+    {
+        help: 'Date to in yyyy-MM-dd format',
+        required: false,
+        defaultValue: ''
+    }
+);
+
+parser.addArgument(
+    [ '-to', '--to' ],
+    {
+        help: 'Date from in yyyy-MM-dd format',
+        required: true
+    }
+);
+
+parser.addArgument(
+    [ '-o', '--output_folder' ],
+    {
+        help: 'Date from in yyyy-MM-dd format',
+        required: false,
+        defaultValue: "./"
+    }
+);
+
+var args = parser.parseArgs();
+
+var language = args["language"];
+var searchTerm = args["search_term"];
+
+var dateTo = moment(args["to"]);
+var dateFrom;
+if(args["from"] === '')
+    dateFrom  = moment().startOf('day');
+else
+    dateFrom = moment(args["from"]);
+
+var slug = slugify(searchTerm);
 
 
-https://twitter.com/search?vertical=news&q=bitcoin%20since%3A2012-11-13%20until%3A2012-11-14&src=typd
 var baseUrl = "https://twitter.com/search?f=tweets&l=%s&q=%s&src=typd";
 
 var job = schedule.scheduleJob("*/5 * * * * *", function () {
 
+    dateFrom.subtract(1, "d");
     var to = moment(dateFrom);
     to.add(1, 'd');
-    dateFrom.subtract(1, "d");
 
     var query = encodeURIComponent(util.format("%s since:%s until:%s",
         searchTerm,
@@ -33,6 +89,8 @@ var job = schedule.scheduleJob("*/5 * * * * *", function () {
     var actualUrl = util.format(baseUrl,
         language,
         query);
+
+    console.log(actualUrl);
 
     var nightmareInstance = nightmare({ show: false });
     nightmareInstance
@@ -71,11 +129,12 @@ var job = schedule.scheduleJob("*/5 * * * * *", function () {
         })
         .end()
         .then(function (result) {
-            fs.writeFile("data/" + dateFrom.format("YYYY-MM-DD") + "-" + slug + "-tweets.json", JSON.stringify(result), function (err) {
+            file = path.join(args["output_folder"],dateFrom.format("YYYY-MM-DD") + "-" + slug + ".json");
+            fs.writeFile(file, JSON.stringify(result), function (err) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("Data for " + dateFrom.format("YYYY-MM-DD") + " saved!");
+                    //console.log("Data for " + dateFrom.format("YYYY-MM-DD") + " saved!");
                 }
             });
         });
@@ -83,7 +142,6 @@ var job = schedule.scheduleJob("*/5 * * * * *", function () {
 
 
     if (dateTo.isAfter(dateFrom)) {
-        console.log("Trabajo terminado");
         job.cancel();
     }
 });
